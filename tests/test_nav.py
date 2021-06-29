fd576352fee27a3612697c5085b6869be90de1d6
Andrew Hick <18572857+andrewhick@users.noreply.github.com>
2021-06-29 13:37:28 +0100

    Finish test to create case

--- tests/test_nav.py
+++ tests/test_nav.py
@@ -2,40 +2,40 @@ import datetime
 import os
 import random
     
-# def test_login_and_navigate(py):
-#     print('Login details: ' + os.environ.get('AMP_USERNAME') + " " + os.environ.get('AMP_PASSWORD'))
-#     py.visit('https://accessibility-monitoring-platform-test.london.cloudapps.digital/')
-#     py.get('.govuk-heading-l').should().contain_text('Login')
-#     # Invalid password
-#     py.get('[name="username"]').type(os.environ.get('AMP_USERNAME'))
-#     py.get('[name="password"]').type('cheese')
-#     py.get('[value="Submit"]').click()
-#     py.get('.govuk-body').should().contain_text('Your username and password didn\'t match.')
-#     py.get('[name="username"]').clear()
-#     # Valid login
-#     # (ideally this would be a repeatable function)
-#     py.get('[name="username"]').type(os.environ.get('AMP_USERNAME'))
-#     py.get('[name="password"]').type(os.environ.get('AMP_PASSWORD'))
-#     py.get('[value="Submit"]').click()
-#     py.get('.govuk-heading-xl').should().contain_text('Dashboard')
-#     # Go to cases page from link in page body
-#     py.get('.govuk-grid-row.dashboard').contains('Cases').click()
-#     py.get('.govuk-heading-xl').should().contain_text('Cases and reports')
-#     py.get('a[href*="/cases/create"').should().be_visible()
-#     py.go('back')
-#     # Go to websites page from link in page body
-#     py.get('.govuk-grid-row.dashboard').contains('Websites').click()
-#     py.get('.govuk-heading-xl').should().contain_text('Query Domain Register')
-#     py.get('#id_service').should().be_visible()
-#     py.go('back')
-#     # Go to account details from header link
-#     py.get('a[href*="/user/account_details/"').click()
-#     py.get('.govuk-heading-l').should().contain_text('Your account details')
-#     py.get('[name="first_name"]').should().be_visible()
-#     py.go('back')
-#     # Sign out
-#     py.get('a[href*="/accounts/logout/"').click()
-#     py.get('.govuk-heading-l').should().contain_text('Login')
+def test_login_and_navigate(py):
+    print('Login details: ' + os.environ.get('AMP_USERNAME') + " " + os.environ.get('AMP_PASSWORD'))
+    py.visit('https://accessibility-monitoring-platform-test.london.cloudapps.digital/')
+    py.get('.govuk-heading-l').should().contain_text('Login')
+    # Invalid password
+    py.get('[name="username"]').type(os.environ.get('AMP_USERNAME'))
+    py.get('[name="password"]').type('cheese')
+    py.get('[value="Submit"]').click()
+    py.get('.govuk-body').should().contain_text('Your username and password didn\'t match.')
+    py.get('[name="username"]').clear()
+    # Valid login
+    # (ideally this would be a repeatable function)
+    py.get('[name="username"]').type(os.environ.get('AMP_USERNAME'))
+    py.get('[name="password"]').type(os.environ.get('AMP_PASSWORD'))
+    py.get('[value="Submit"]').click()
+    py.get('.govuk-heading-xl').should().contain_text('Dashboard')
+    # Go to cases page from link in page body
+    py.get('.govuk-grid-row.dashboard').contains('Cases').click()
+    py.get('.govuk-heading-xl').should().contain_text('Cases and reports')
+    py.get('a[href*="/cases/create"').should().be_visible()
+    py.go('back')
+    # Go to websites page from link in page body
+    py.get('.govuk-grid-row.dashboard').contains('Websites').click()
+    py.get('.govuk-heading-xl').should().contain_text('Query Domain Register')
+    py.get('#id_service').should().be_visible()
+    py.go('back')
+    # Go to account details from header link
+    py.get('a[href*="/user/account_details/"').click()
+    py.get('.govuk-heading-l').should().contain_text('Your account details')
+    py.get('[name="first_name"]').should().be_visible()
+    py.go('back')
+    # Sign out
+    py.get('a[href*="/accounts/logout/"').click()
+    py.get('.govuk-heading-l').should().contain_text('Login')
 
 def test_create_case(py):
     # Test that a case can be created, is searchable, and increments the number of active cases
@@ -61,8 +61,8 @@ def test_create_case(py):
     py.get('[value="simple"]').should().be_checked()
     py.get('[name="home_page_url"]').type("https://www.andrewhick.com/")
     # Generate random organisation name
-    test_organisation = "Case " + str(random.randint(0, 999999))
-    py.get('[name="organisation_name"]').type("Case " + test_organisation)
+    test_organisation = "Organisation " + str(random.randint(0, 999999))
+    py.get('[name="organisation_name"]').type(test_organisation)
     print("Case name: " + test_organisation)
     py.get('[value="public"]').check()
     py.get('select[name="sector"]').select('Local Government')
@@ -93,16 +93,27 @@ def test_create_case(py):
     py.get('[value="Search"]').click()
 
     # Check that case appears in results with case number and today's date
+    py.get('.govuk-heading-m').should().contain_text('1 cases found')
     py.get('.govuk-table').should().contain_text('Case #' + str(case_number))
     py.get('.govuk-table').should().contain_text(test_organisation)
     py.get('.govuk-table').should().contain_text(datetime.datetime.now().strftime('%d/%m/%Y'))
-    py.get('.govuk-heading-m').should().contain_text('1 cases found')
     # Access the case
     py.get('a[href*="/view"]').click()
-    # TODO Unlist case
-    # TODO Check that case no longer appears in results
+    py.get('.govuk-heading-xl').should().contain_text('View case #' + str(case_number))
+    py.get('.govuk-table').should().contain_text(test_organisation)
+
+    # Unlist case
+    py.get('a[href*="/archive-case"').click()
+    # Check that number of cases has gone back to original value
+    cases_text = py.get('.govuk-heading-m').text()
+    new_active_cases = int(cases_text.split(' ')[0])
+    print("Number of cases: " + str(new_active_cases))
+    assert new_active_cases == original_active_cases
+    # Check that newly-created case is no longer searchable
+    py.get('#id_organisation').type(test_organisation)
+    py.get('[value="Search"]').click()
+    py.get('.govuk-heading-m').should().contain_text('0 cases found')
     py.wait(use_py=True).sleep(2)
 
-# todo: finish creating case
 # stretch goal: check error messages
 # stretch goal: add axe test
